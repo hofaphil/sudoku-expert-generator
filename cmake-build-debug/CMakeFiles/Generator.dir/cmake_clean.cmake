894f06cdd2fc35e1858fcd62547cdd78ca790e14
Philipp Hofer <philipp.hofer@aol.com>
2021-04-18 18:56:11 +0200

    added git

---
+++ cmake-build-debug/CMakeFiles/Generator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Generator.dir/Block.c.o"
+  "CMakeFiles/Generator.dir/Number.c.o"
+  "CMakeFiles/Generator.dir/Sudoku.c.o"
+  "CMakeFiles/Generator.dir/main.c.o"
+  "Generator"
+  "Generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
